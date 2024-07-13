5415b8ad8ce5a7d0bcdc728d95e9ebef440c48d0
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-07-13 11:56:39 +0500

    feat: add endpoint for fetching companies

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,23 @@
-import express from "express";
-import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
+import express from "express";
+import "express-async-errors";
 
+import cors, { CorsOptions } from "cors";
+import { listCompanyRouter } from "routes/company/list-company";
+import { changePasswordRouter } from "./routes/auth/change-password";
+import { forgotPasswordRouter } from "./routes/auth/forgot-password";
+import { newPasswordRouter } from "./routes/auth/new-password";
 import { signinRouter } from "./routes/auth/signin";
 import { signoutRouter } from "./routes/auth/signout";
 import { signupRouter } from "./routes/auth/signup";
-import { changePasswordRouter } from "./routes/auth/change-password";
-import { forgotPasswordRouter } from "./routes/auth/forgot-password";
 import { verifyForgotPasswordTokenRouter } from "./routes/auth/verify-forgot-pass-token";
-import { newPasswordRouter } from "./routes/auth/new-password";
-import cors, { CorsOptions } from "cors";
-import { Company } from "./models/Company";
-import { listVisitorRouter } from "./routes/visitor/list-visitor";
-import { createVisitorRouter } from "./routes/visitor/create-visitor";
-import { listVisitsRouter } from "./routes/visitor/list-visit";
-import { createVisitRouter } from "./routes/visitor/create-visit";
 import { checkInVisitRouter } from "./routes/visitor/check-in";
 import { checkOutVisitRouter } from "./routes/visitor/check-out";
+import { createVisitRouter } from "./routes/visitor/create-visit";
+import { createVisitorRouter } from "./routes/visitor/create-visitor";
+import { listVisitsRouter } from "./routes/visitor/list-visit";
+import { listVisitorRouter } from "./routes/visitor/list-visitor";
 
 const dotenv = require("dotenv").config();
 
@@ -43,22 +43,23 @@ const corsOptions: CorsOptions = {
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 
-app.get(`/company`, async (req, res) => {
-  const companies = await Company.find();
-  res.status(200).json(companies);
-});
+// app.get(`/company`, async (req, res) => {
+//   const companies = await Company.find();
+//   res.status(200).json(companies);
+// });
 
-app.post(`/company`, (req, res) => {
-  const company = new Company({
-    ...req.body,
-    nextPaymentDate: new Date(),
-    lastPaymentDate: new Date(),
-  });
-  company.save();
-  res.status(201).send(company);
-});
+// app.post(`/company`, (req, res) => {
+//   const company = new Company({
+//     ...req.body,
+//     nextPaymentDate: new Date(),
+//     lastPaymentDate: new Date(),
+//   });
+//   company.save();
+//   res.status(201).send(company);
+// });
 
 app.use(signupRouter);
+app.use(listCompanyRouter);
 app.use(signinRouter);
 app.use(changePasswordRouter);
 app.use(signoutRouter);
@@ -72,11 +73,11 @@ app.use(createVisitRouter);
 app.use(checkInVisitRouter);
 app.use(checkOutVisitRouter);
 
-app.use("/", async (req, res) => {
+app.use("/", (req, res) => {
   res.send("GateKipa Backend API");
 });
 
-app.all("*", async (req, res, next) => {
+app.all("*", (req, res, next) => {
   throw new Error("Route not found");
 });
 
---
+++ src/routes/company/list-company.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response } from "express";
+import { Company } from "models/Company";
+import { ApiResponseDto } from "../../dto/api-response.dto";
+
+const router = express.Router();
+
+router.get("/api/company/", async (req: Request, res: Response) => {
+  try {
+    const companies = await Company.find()
+      .select("_id companyCode name emailAddress")
+      .sort({ createdAt: -1 });
+
+    return res
+      .status(200)
+      .send(
+        new ApiResponseDto(false, "Visits fetched successfully", companies, 200)
+      );
+  } catch (error) {
+    console.error("Error occurred during list-company", error);
+    return res
+      .status(500)
+      .send(
+        new ApiResponseDto(
+          true,
+          "Something wen't wrong while fetching company",
+          [],
+          500
+        )
+      );
+  }
+});
+
+export { router as listCompanyRouter };
