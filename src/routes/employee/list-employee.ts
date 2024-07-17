dcbd6d78b4919b76cb61b61b4d76cd086d9f0601
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-07-17 12:58:24 +0500

    fix: add requireAuth middleware in fetching employees

--- src/routes/employee/list-employee.ts
+++ src/routes/employee/list-employee.ts
@@ -2,50 +2,56 @@ import express, { Request, Response } from "express";
 import { Employee } from "../../models/Employee";
 import { ApiResponseDto } from "../../dto/api-response.dto";
 import { Types } from "mongoose";
+import { requireAuth } from "middlewares/require-auth.middleware";
 
 const router = express.Router();
 
-router.get("/api/employee/", async (req: Request, res: Response) => {
-  try {
-    if (!req?.user) {
+router.get(
+  "/api/employee/",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    console.log("req?.user :>> ", req?.user);
+    try {
+      if (!req?.user) {
+        return res
+          .status(400)
+          .send(
+            new ApiResponseDto(true, "Company information is required", [], 400)
+          );
+      }
+
+      const { companyId } = req?.user;
+
+      const employees = await Employee.find({
+        companyId: new Types.ObjectId(companyId),
+      }).sort({
+        createdAt: -1,
+      });
+
       return res
-        .status(400)
+        .status(200)
         .send(
-          new ApiResponseDto(true, "Company information is required", [], 400)
+          new ApiResponseDto(
+            false,
+            "Employees fetched successfully",
+            employees,
+            200
+          )
+        );
+    } catch (error) {
+      console.error("Error occurred during list-employee", error);
+      return res
+        .status(500)
+        .send(
+          new ApiResponseDto(
+            true,
+            "Something wen't wrong while fetching employees",
+            [],
+            500
+          )
         );
     }
-
-    const { companyId } = req?.user;
-
-    const employees = await Employee.find({
-      companyId: new Types.ObjectId(companyId),
-    }).sort({
-      createdAt: -1,
-    });
-
-    return res
-      .status(200)
-      .send(
-        new ApiResponseDto(
-          false,
-          "Employees fetched successfully",
-          employees,
-          200
-        )
-      );
-  } catch (error) {
-    console.error("Error occurred during list-employee", error);
-    return res
-      .status(500)
-      .send(
-        new ApiResponseDto(
-          true,
-          "Something wen't wrong while fetching employees",
-          [],
-          500
-        )
-      );
   }
-});
+);
 
 export { router as listEmployeeRouter };
